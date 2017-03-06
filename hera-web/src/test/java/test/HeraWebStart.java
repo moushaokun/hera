361823181fc4ce49df5a4fbd48247841cb03f510
apel-apel <530328032@qq.com>
2017-03-06 14:29:39 +0800

    c

--- hera-web/src/test/java/test/HeraWebStart.java
+++ hera-web/src/test/java/test/HeraWebStart.java
@@ -2,7 +2,7 @@ package test;
 
 import org.apel.gaia.container.boot.PlatformStarter;
 
-public class TestStart {
+public class HeraWebStart {
 	
 	public static void main(String[] args) {
 		PlatformStarter.start(args);
